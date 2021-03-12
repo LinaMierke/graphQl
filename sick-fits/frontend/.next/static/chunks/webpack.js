9316b03ac127b6ee445f0b594db3b33abb9f60d5
lina mierkE <lina_maria89@hotmail.com>
2021-03-12 17:08:15 -0500

    global and inner styles

--- sick-fits/frontend/.next/static/chunks/webpack.js
+++ sick-fits/frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c4787976d3f516acc6f0";
+/******/ 	var hotCurrentHash = "d275a04ea751023a7cf1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Nav from "../components/Nav";
 import styled from "styled-components";
 
-const Logo = styled.h1 `
+const Logo = styled.h1`
   background: red;
   font-size: 4rem;
   margin-left: 2rem;
@@ -16,21 +16,19 @@ const Logo = styled.h1 `
     padding: 0.5rem 1rem;
   }
 `;
-const HeaderStyles = styled.header `
-  .bar{
-      border-bottom: 10px solid var(--black, black);
-      display: grid;
-      grid-template-columns: auto 1fr;
-      justify-content: space-between;
-      align-items: center;
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
   }
   .sub-bar {
-      display: grid;
-      grid-template-columns: 1fr auto;
-      border-bottom: 1px solid var(--black, black);
-
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
   }
-
 `;
 
 export default function Header() {
