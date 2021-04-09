ce3187c880ab74735aed1337b41fd6c3ea0d1cc6
lina mierkE <lina_maria89@hotmail.com>
2021-04-09 17:26:34 -0400

    server style sheets

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5c423af4069111fc1285";
+/******/ 	var hotCurrentHash = "5d1d291a8137373e2fad";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,9 +1,18 @@
 import Document, { Html, Head, NextScript, Main } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 //this is only few places where we will use class. dont freak out
 // we need to import document and html, this is from the next.js documentation =>
 /*A custom Document is commonly used to augment your application's <html> and <body> tags. This is necessary because Next.js pages skip the definition of the surrounding document's markup.*/
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return {...page, styleTags}
+  }
   render() {
     return (
       <Html lang="en">
