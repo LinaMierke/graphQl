bf64e8d0b78ad11d081fc4c11e760c81f6ec6e33
lina mierkE <lina_maria89@hotmail.com>
2021-04-09 09:49:06 -0400

    nav and header

--- frontend/components/page.js
+++ frontend/components/page.js
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types";
+import Header from "./Header";
 
 // so here is a parent component and here is with the children will pass, so when I use the <Page> tag in other documment like the index, there I typed some stuff and if i want it to pass here temporary(no won't pass in every page that the <Page> tag is name, just where the children are .. remember: tuvo hijos fuera del matrimonio en otra casa(index.js), y ahora aca en su principal casa tiene que traerlos a mostrarlos y a q se los acepten, pero solo van  a funcionar alla en esa casa no en todas las casas donde vaya de visita)
 export default function Page({ children, cool }) {
   return (
+    //this <Header> is the component create ut , and it will be shown in all th epages
     <div>
+      <Header> </Header>
       <h1>
         I am the page component! children because Im smaller and i belong
         somewhere:$
@@ -17,7 +20,7 @@ export default function Page({ children, cool }) {
   );
 }
 
-// this is a next.js library for passing props so we are make sure it will work, how? well first, we need to import it OFCOURSE my pez, luego aca creams este con el nombre de la funcion y el tipo d eprops, puede ser boolena, array, string etc, y asi el documento sabe que tipo de prop es y estar seguros. 
+// this is a next.js library for passing props so we are make sure it will work, how? well first, we need to import it OFCOURSE my pez, luego aca creams este con el nombre de la funcion y el tipo d eprops, puede ser boolena, array, string etc, y asi el documento sabe que tipo de prop es y estar seguros.
 Page.propTypes = {
   cool: PropTypes.string,
   children: PropTypes.any,
